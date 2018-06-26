070ddcc5c04e0b44ce92c8694d5f9efed650eefc
Kaentos <miguel30317@esfundao.pt>
2018-06-26 16:29:44 +0100

    Changed the "output" of JSON to make it show the name of columns

--- ScriptToJSON.py
+++ ScriptToJSON.py
@@ -12,48 +12,48 @@ class Games(Resource):
     def get(self):
         db = db_connect.connect()
         query = db.execute("SELECT * from FifaWorldCup")
-        return {'Games': [i for i in query.cursor.fetchall()]}
+        return {'Games': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
 
 class Teams(Resource):
     def get(self):
         db = db_connect.connect()
         query = db.execute("SELECT DISTINCT TeamHome from FifaWorldCup ORDER BY TeamHome")
-        return {'Group': [i for i in query.cursor.fetchall()]}
+        return {'Group': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
 
 class Stadiums(Resource):
     def get(self):
         db = db_connect.connect()
         query = db.execute("SELECT DISTINCT Location from FifaWorldCup ORDER BY Location")
-        return {'Stadiums': [i for i in query.cursor.fetchall()]}
+        return {'Stadiums': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
 
 class Groups(Resource):
     def get(self):
         db = db_connect.connect()
         query = db.execute("SELECT DISTINCT MatchGroup from FifaWorldCup ORDER BY MatchGroup")
-        return {'Groups': [i for i in query.cursor.fetchall()]}
+        return {'Groups': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
 
 class GroupInfo(Resource):
     def get(self,g):
         db = db_connect.connect()
         g = '%' + g + '%'
         query = db.execute("SELECT * from FifaWorldCup WHERE MatchGroup LIKE (?)", (g,))
-        return {"Group Info": [i for i in query.cursor.fetchall()]}
+        return {"Group Info": [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
 
 class TeamInfo(Resource):
     def get(self,t):
         db = db_connect.connect()
         t = '%' + t + '%'
         query = db.execute("SELECT * from FifaWorldCup WHERE TeamHome LIKE (?) OR TeamAway LIKE (?)", (t,t,))
-        return {"Team Info": [i for i in query.cursor.fetchall()]}
+        return {"Team Info": [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
 
 class StadiumInfo(Resource):
     def get(self, s):
         db = db_connect.connect()
         s = '%' + s + '%'
         query = db.execute("SELECT * from FifaWorldCup WHERE Location LIKE  (?)", (s,))
-        return {'Games in ' + s: [i for i in query.cursor.fetchall()]}
+        return {'Stadium games': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
 
-api.add_resource(Games, '/WorldGroups2018')
+api.add_resource(Games, '/Games')
 api.add_resource(Teams, '/Teams')
 api.add_resource(Stadiums, '/Stadiums')
 api.add_resource(Groups, '/Groups')
