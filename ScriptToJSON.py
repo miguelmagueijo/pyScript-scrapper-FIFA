16bea08c102d830329b27738b8dca02378ec1a36
Kaentos <miguel30317@esfundao.pt>
2018-06-26 11:36:58 +0100

    Changed names  on ScriptToJson

--- ScriptToJSON.py
+++ ScriptToJSON.py
@@ -8,13 +8,13 @@ db_connect = create_engine('sqlite:///db.sqlite3')
 app = Flask(__name__)
 api = Api(app)
 
-class Employees(Resource):
+class Games(Resource):
     def get(self):
-        conn = db_connect.connect() # connect to database
-        query = conn.execute("select * from FifaWorldCup") # This line performs query and returns json result
+        db = db_connect.connect()
+        query = db.execute("select * from FifaWorldCup")
         return {'Games:': [i for i in query.cursor.fetchall()]}        
 
-api.add_resource(Employees, '/WorldGroups2018') # Route_1
+api.add_resource(Games, '/WorldGroups2018')
 
 if __name__ == '__main__':
      app.run(port='5002')
